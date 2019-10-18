1e93340fbdfdf128a6ec2e2d9c0991c3587fff2f
excellent-application-0463 <jeffrey.chan-1@outlook.com>
2019-10-18 04:49:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,13 @@ var playlist = {}
 var playlist = {
   artist: "song",
 }
+
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
   return playlist
 }
